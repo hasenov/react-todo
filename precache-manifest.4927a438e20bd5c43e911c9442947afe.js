8a90d08d0b664efa34a3938586182e9566037bad
Aidar <aidarwebdev@gmail.com>
2020-06-29 01:47:14 +0600

    Updates

---
+++ precache-manifest.4927a438e20bd5c43e911c9442947afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b5a428c81f416dbc48373109067825",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "d1b2ba47436590aa8cba",
+    "url": "/react-todo/static/css/main.16e16532.chunk.css"
+  },
+  {
+    "revision": "f95bc86199a85694e632",
+    "url": "/react-todo/static/js/2.b0bf4276.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.b0bf4276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b2ba47436590aa8cba",
+    "url": "/react-todo/static/js/main.d2b73dec.chunk.js"
+  },
+  {
+    "revision": "5190324ecb34ab2d5bd4",
+    "url": "/react-todo/static/js/runtime-main.0dd0b0c2.js"
+  }
+]);
